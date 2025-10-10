4fae43303294f2d4a57825a5259690dd8e95f8b3
Karan Kalia <karansharma020710@gmail.com>
2025-10-10 19:33:16 +0530

    updated

--- mlp/mlp.py
+++ mlp/mlp.py
@@ -1,4 +1,7 @@
 import numpy as np
 from mlp.activations import sigmoid, sigmoid_derivative
-from mlp.losses import MeanSquaredError, CrossEntropyLoss
+from mlp.losses import mse, binary_cross_entropy, categorical_cross_entropy
+from mlp.utils import one_hot, accuracy, dropout
+
+
 
--- mlp/utils.py
+++ mlp/utils.py
@@ -2,3 +2,10 @@ import numpy as np
 
 def one_hot(y,num_classes):
     return np.eye(num_classes)[y]
+
+def accuracy(y_true,y_pred):
+    return np.mean(y_true == y_pred)
+
+def dropout(H,p = 0.5):
+    mask = (np.random.randn(*H.shape) > p)/(1 - p)
+    return H * mask
