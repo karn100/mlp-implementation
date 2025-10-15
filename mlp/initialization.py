ba9ce0a4f77a24fbed8356a09a168130f66f70e3
Karan Kalia <karansharma020710@gmail.com>
2025-10-15 19:42:38 +0530

    updated initialisations

--- mlp/initialization.py
+++ mlp/initialization.py
@@ -1,4 +1,7 @@
 import numpy as np
 
+def xavior_init(n_in:int,n_out:int)-> np.darray:
+    return np.random.rand(n_in,n_out) * np.sqrt(2 / (n_in + n_out))
+
 def he_init(n_in:int,n_out:int)-> np.darray:
-    
+    return np.random.randn(n_in,n_out) * np.sqrt(2/n_in)
