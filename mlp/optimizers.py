695fa3e350a516a1148d038c6fe62e9b665bcbe5
Karan Kalia <karansharma020710@gmail.com>
2025-10-22 14:18:48 +0530

    updated

--- mlp/optimizers.py
+++ mlp/optimizers.py
@@ -1,5 +1,21 @@
 import numpy as np
+import torch
+from torch.optim import Optimizer
 
-class MomentumOptimizer:
-    def __init__(self,lr):
-        self.lr = lr
+class MomentumOptimizer(Optimizer):
+    def __init__(self,params,lr,mom_cof = 0.9,nesterov = False):
+
+        default = dict(lr = lr,mom_cof = mom_cof,nesterov = nesterov)
+        super().__init__(params,default)
+    
+    def step(self,closure = None):
+        loss = None
+        if closure:
+            loss = closure()
+        
+        for group in self.param_groups:
+            lr = group['lr']
+            mom_cof = group['mom_cof']
+            nesterov = group['nesterov']
+            for  p in group['params']:
+            
